2690db8907e615b88f9c3609e85c22645cdaefd5
blackrabbit99 <myzlio@gmail.com>
2012-12-02 19:21:25 +0400

    add partners

--- src/sites/kharkivjs.com/protected/views/site/pages/partners.php
+++ src/sites/kharkivjs.com/protected/views/site/pages/partners.php
@@ -12,6 +12,22 @@
         <a target="_blank" href="http://mediasapiens.co/"><strong><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo.jpeg" alt="Dev-Pro logo" class="alignleft" /></strong></a>
         <p>Small, neat and flexible international team with modern stack technologies and ambitions. Riding own way for the full circle solutions and service-base products.</p>
     </section>
+	 <div class="title">
+        <div class="wrap">
+            <span class="blank"></span><h2>Conference partner</h2><span class="blank"></span>
+        </div>
+    </div>
+     <section>
+        <a target="_blank" href="http://www.epam-group.ru/"><strong><img width="405" height="123" src="<?php echo Yii::app()->request->baseUrl; ?>/images/partners/epam.png" alt="Epam Group logo" class="alignleft" /></strong></a>
+        <p>EPAM Systems — крупнейшая ИТ-компания, надежный партнер для сложных и масштабных проектов</p> 
+        <p>В постоянно меняющихся рыночных условиях надежность становится ключевым критерием выбора ИТ-компании. Обеспечить адекватную поддержку динамично развивающемуся бизнесу способен только тот партнер, который гарантирует стабильность, гибкость, качество всех процессов разработки программного обеспечения и высокую квалификацию специалистов, задействованных в проектах.
+        </p>
+        
+    </section>
+	<section>
+        <a target="_blank" href="http://scrumtools.com.ua/"><strong><img width="405" height="123" src="<?php echo Yii::app()->request->baseUrl; ?>/images/partners/scrumtools.png" alt="Scrumtools logo" class="alignleft" /></strong></a>
+        <p>Development team of the same name product oriented to agile-based project development and Front-End outsource. We make the easiest tool to manage Scrum processes, build complicated web applications on JavaScript, PHP, HTML5 and trying to bring our Scrum philosophy to others.</p>        
+    </section>
     <div class="title">
         <div class="wrap">
             <span class="blank"></span><h2>Hosting partner</h2><span class="blank"></span>
@@ -23,7 +39,7 @@
         <p>Мы внедрили cотни проектов различной степени сложности для клиентов по всему миру. Нам нравится программировать, проектировать удобные интерфейсы, рисовать яркие сайты и создавать эффектную флеш-анимацию. 
         Мы постоянно ведем поиск единомышленников, для которых создание веб-сайтов является чем-то большим, нежели просто увлечением. Мы гордимся каждой своей разработкой, будь то создание одностраничного веб-сайта или же работа в сотрудничестве с нашими партнерами, – ведущими студиями дизайна США и Великобритании над масштабными проектами для таких всемирно известных брендов, как Procter & Gamble, NFL, Seagate, Nike, Sotheby’s, и многих других. 
         </p>
-        <p>На сегодняшний день, наш коллектив насчитывает более 150 человек. Мы делаем всё возможное для создания таких условий, при которых от работы получают максимум удовольствия. Потому что Zfort Group – Дружественный Работодатель.</p>
+        
     </section>
     <div class="title">
         <div class="wrap">
